97b6a1c8c5c4c465d894bf94f88acab64416d9ae
LONHik4 <gaynullin.dmitriy@gmail.com>
2021-12-09 03:38:53 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+# -*- coding: cp1251 -*-
+import pandas as pd
+from multiprocessing import Pool
+from time import time
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+def calculate(*data):
+    return sum(data)
+
+
+df = pd.read_csv("data.csv", sep=",", encoding='windows-1251')
+print(df["Активные случаи"].tolist())
+calculate()
+
+if __name__=="__main__":
+    time_1 = time()
+    lst=[]
+    for i in range(1, 5):
+        with Pool(i) as p:
+            print(p.apply(calculate, df["Активные случаи"].tolist()))
+        a=time() - time_1
+        lst.append(a)
+        print(time() - time_1)
+    print(lst)
+
+    fig=plt.figure(figsize=(30, 5))
+    plt.ylabel('y')
+    plt.xlabel('x')
+    plt.title('y=sinc(x)')
+    plt.scatter(list(range(1,5)),lst, color='teal', marker='o', linewidths=2, facecolors='none')
+    plt.show()
