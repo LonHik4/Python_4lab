ec6459fe5db89f34563f79fe3ceb21d664d02f8d
LONHik4 <gaynullin.dmitriy@gmail.com>
2021-12-09 04:10:37 +0400

    LVL UP

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# -*- coding: cp1251 -*-
+# -*- coding: UTF-8 -*-
 import pandas as pd
 from multiprocessing import Pool
 from time import time
@@ -7,27 +7,27 @@ import numpy as np
 
 
 def calculate(*data):
-    """сумма всех активных случаев"""
+    """СЃСѓРјРјР° РІСЃРµС… Р°РєС‚РёРІРЅС‹С… СЃР»СѓС‡Р°РµРІ"""
     a=list(data)
     return sum(a)
 
 
 df = pd.read_csv("data.csv", sep=",", encoding='windows-1251')
-print(df["Активные случаи"].tolist())
+print(df["РђРєС‚РёРІРЅС‹Рµ СЃР»СѓС‡Р°Рё"].tolist())
 calculate()
 
 if __name__=="__main__":
-    """находим время работы при разном количестве процессов и выводим массив из времен"""
+    """РЅР°С…РѕРґРёРј РІСЂРµРјСЏ СЂР°Р±РѕС‚С‹ РїСЂРё СЂР°Р·РЅРѕРј РєРѕР»РёС‡РµСЃС‚РІРµ РїСЂРѕС†РµСЃСЃРѕРІ Рё РІС‹РІРѕРґРёРј РјР°СЃСЃРёРІ РёР· РІСЂРµРјРµРЅ"""
     time_1 = time()
     lst=[]
     for i in range(1, 5):
         with Pool(i) as p:
-            print(p.apply(calculate, df["Активные случаи"].tolist()))
+            print(p.apply(calculate, df["РђРєС‚РёРІРЅС‹Рµ СЃР»СѓС‡Р°Рё"].tolist()))
         a=time() - time_1
         lst.append(a)
         print(time() - time_1)
     print(lst)
-    """Создаем точечную диаграмму используя время """
+    """РЎРѕР·РґР°РµРј С‚РѕС‡РµС‡РЅСѓСЋ РґРёР°РіСЂР°РјРјСѓ РёСЃРїРѕР»СЊР·СѓСЏ РІСЂРµРјСЏ """
     fig=plt.figure(figsize=(10, 10))
     plt.ylabel('y')
     plt.xlabel('x')
